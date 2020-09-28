49e81b87764fc70a41952336a0604f8c2cd26820
Nürek <69650894+Alex8Navas@users.noreply.github.com>
2020-09-28 22:48:48 +0200

    Add files via upload

---
+++ Python_PI/KalPak/KalGeneric.py
@@ -0,0 +1,25 @@
+# Cálculos Genéricos. 
+
+def sumar(n1, n2):
+
+	print("El resultado de la suma de", n1, "más", n2, "es:", n1+n2)
+
+def restar(n1, n2):
+
+	print("El resultado de la resta de", n1, "menos", n2, "es:", n1-n2)
+
+def producto(n1, n2):
+
+	print("El resultado del producto de", n1, "por", n2, "es:", n1*n2)
+
+def divis(n1, n2):
+
+	print("El resultado de la división de", n1, "entre", n2, "es:", n1/n2)
+
+def potencia(n1, n2):
+
+	print("El resultado de la potencia de", n1, "elevado a", n2, "es:", n1**n2)
+
+def redond(n1):
+
+	print("El valor redondeado de", n1, "es:", round(n1))
---
+++ Python_PI/KalPak/KalSubPak/BasicsKal.py
@@ -0,0 +1,17 @@
+# Cálculos Básicos 
+
+def sumar2(n1, n2):
+
+	print("El resultado de la suma de", n1, "más", n2, "es:", n1+n2)
+
+def restar2(n1, n2):
+
+	print("El resultado de la resta de", n1, "menos", n2, "es:", n1-n2)
+
+def producto2(n1, n2):
+
+	print("El resultado del producto de", n1, "por", n2, "es:", n1*n2)
+
+def divis2(n1, n2):
+
+	print("El resultado de la división de", n1, "entre", n2, "es:", n1/n2)
