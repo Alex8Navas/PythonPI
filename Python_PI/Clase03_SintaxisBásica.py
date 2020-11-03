07c7e2a7e2956f170c1811a86510679d9406805d
Kattaniss <69650894+Alex8Navas@users.noreply.github.com>
2020-11-03 12:10:58 +0100

    Add files via upload

---
+++ Python_PI/Clase03_SintaxisBásica.py
@@ -0,0 +1,13 @@
+# Clase 3. Curso Píldoras Informáticas.
+
+Hey = "Mi nombre es John"
+print(Hey)
+
+# \ sirve para pegar un salto pero seguir programando
+Hey = "Mi nombre \
+es John"
+print(Hey)
+
+for i in range(5):
+	print(i)
+
